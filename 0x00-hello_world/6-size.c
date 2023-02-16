3573f69bcc48d53a4e8da30233fc3fbd14c6919f
mddon <mmrealworld@yahoo.com>
2023-02-16 07:24:16 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - A program that prints the size of varios computer types
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf(
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(f);
+return (0);
+}
