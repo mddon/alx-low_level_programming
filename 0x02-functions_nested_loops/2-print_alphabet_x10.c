834818f57c8a9a0e2418403b2ccf9e9eaa209ef4
mddon <mmrealworld@yahoo.com>
2023-02-22 01:43:26 -0800

    alphabet_10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
- * print_alphabet_10x - function that prints 10x alphabets
- * Return: always 0;
+ * print_alphabet_x10 - function that prints 10x alphabets
+ *
+ * Return: always 0
  */
 
 void print_alphabet_x10(void)
@@ -12,8 +12,7 @@ void print_alphabet_x10(void)
 
 	for (i = 1 ; i <= 10 ; i++)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 		_putchar('\n');
-	}
 }
