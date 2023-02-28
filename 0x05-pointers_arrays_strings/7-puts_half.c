43183a164684684a8a53c4ae2da65e0585745222
mddon <mmrealworld@yahoo.com>
2023-02-28 14:27:42 -0800

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,14 @@
-#include "main.h" 
-/** 
-* puts_half - prints half of a string. 
-* @str: input string. 
-* Return: no return. 
+#include "main.h"
+
+/**
+* puts_half - prints half of a string.
+* @str: input string.
+* Return: no return.
 */
+
 void puts_half(char *str)
-{	
-	int count = 0, i; 	
+{
+	int count = 0, i;
 
 	while (count >= 0)
 	{
