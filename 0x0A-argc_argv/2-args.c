1321141dad3b027d244b007ad12a7e034da4fb43
Miracle Obinna <mmrealworld@yahoo.com>
2023-06-11 18:11:25 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+
+	exit(EXIT_SUCCESS);
+
+}
