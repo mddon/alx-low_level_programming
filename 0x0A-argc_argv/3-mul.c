e8b88c7edcfbffb59e0ab0c3d8b3fb57a5508a0e
Miracle Obinna <mmrealworld@yahoo.com>
2023-06-11 18:39:19 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 1, b;
+	if(argc == 3)
+	{
+		for (b = 1; b < argc; b++)
+			a = a * strtol(argv[b], NULL, 10);
+		printf("%d\n", a);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
