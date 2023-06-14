83fd625dbda77b184a6f2651eefeab1e1bd76414
Miracle Obinna <mmrealworld@yahoo.com>
2023-06-14 16:20:38 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_number();
+/**
+ * main - Prints the addition of positive numbers
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int a, k, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (k = 0; argv[a][k]; k++)
+		{
+			if (argv[a][k] < '0' || argv[a][k] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
