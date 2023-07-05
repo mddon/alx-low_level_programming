09b6989f4ccedd2d0f341464771dbb2a1bf9715f
Miracle Obinna <mmrealworld@yahoo.com>
2023-07-05 15:16:52 -0700

    more singly

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t new_Nds = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nnodes++;
+		new_Nds++;
 	}
-	return (nnodes);
+	return (new_Nds);
 }
