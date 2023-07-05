6938b60bafbae8aa5c54bdab4aefdc04901334ae
Miracle Obinna <mmrealworld@yahoo.com>
2023-07-05 12:34:34 -0700

    more_singly

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a list.
@@ -6,6 +7,7 @@
  *
  * Return: this is the numbers of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
