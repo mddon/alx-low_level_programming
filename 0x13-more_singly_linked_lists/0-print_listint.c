ff0f0b673c7b4c8b649e41fd04dbfa68631e5533
Miracle Obinna <mmrealworld@yahoo.com>
2023-07-05 13:31:27 -0700

    more_singly

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
-	
+
 	for (; h != NULL; h = h->next)
 	{
 		printf("%d\n", h->n);
 		nnodes++;
 	}
-	
-	return nnodes;
+
+	return (nnodes);
 }
 
