142dae2498943e618be7c3f2ec8f94d24aaa645b
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-17 12:52:13 -0700

    linked_list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,11 +12,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
-	do {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
-	} while (h != NULL);
+	}
 
 	return (nodes);
 }
