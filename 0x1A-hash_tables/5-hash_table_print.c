2d660aec2949cb9989988fc78cb01e526ebb5231
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-08 19:54:02 -0700

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,4 @@
-#include "hash_tables.h"
+#include "./hash_tables.h"
 
 /**
  * hash_table_print - Prints a hash table.
@@ -9,34 +9,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *current_node;
-	unsigned long int k;
+	hash_node_t *node;
+	unsigned long int i;
 	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
-	{
 		return;
-	}
 
 	printf("{");
-	k = 0;
-	while (k < ht->size)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[k] != NULL)
+		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
-				printf(",");
-			current_node = ht->array[k];
-			while (current_node != NULL)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				printf("'%s': '%s'", current_node->key, current_node->value);
-				current_node = current_node->next;
-				if (current_node != NULL)
-					printf(",");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
 			}
 			comma_flag = 1;
 		}
-		k++;
 	}
 	printf("}\n");
 }
