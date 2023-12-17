5848bd84c80e132ee18d5d153d465a49683360aa
Miracle Obinna <mmrealworld@yahoo.com>
2023-12-16 18:13:19 -0800

    update

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,20 +12,20 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t index, k, m;
+	size_t index, js, bs;
 	listint_t *prev;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	m = (size_t)sqrt((double)size);
+	bs = (size_t)sqrt((double)size);
 	index = 0;
-	k = 0;
+	js = 0;
 
 	do {
 		prev = list;
-		k++;
-		index = k * m;
+		js++;
+		index = js * bs;
 
 		while (list->next && list->index < index)
 			list = list->next;
